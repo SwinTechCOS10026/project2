501ca825867855d1698807673de8791e7f8fbcee
Chea-Roelink <105913750@student.swin.edu.au>
2025-05-26 21:46:44 +1000

    Added all the enhancement changes to the enhancement changes

--- project2/enhancements.php
+++ project2/enhancements.php
@@ -1,17 +1,42 @@
 <?php include("header.inc"); ?> 
 <?php include("nav.inc"); ?> 
 
-<h2 style="text-align:center;">Enhancements Showcase</h2>
+<h2 id="enhancements-heading">Enhancements Showcase</h2>
 
-<div style="display:flex; justify-content:space-around; margin:20px;">
-    <div style="width:150px; height:150px; background-color:#FFCCCC; border:1px solid #999; text-align:center; line-height:150px;">
-        Box 1
+<div id="enhancements-contents">
+    <div id="enhancement-item1">
+        <h3>Expression of Interest sorting</h3>
+        <img src="images/enhancements_img/eoi_sorting.png" alt="Expression of Interest Sorting and Searching User Interface" id="enhancement-img1"></img>
+        <p id="enhancement-text1">Provide the manager with the ability to select the field on which to sort the order in
+            which the EOI records are displayed.</p><br>
+        <div class='apply-btn'><a class='btn-gold' href='login.php' target='_blank' role='button'>Login Now</a></div>
     </div>
-    <div style="width:150px; height:150px; background-color:#CCFFCC; border:1px solid #999; text-align:center; line-height:150px;">
-        Box 2
+    <div id="enhancement-item2">
+        <h3>Managerial registration page</h3>
+        <img src="images/enhancements_img/manager_registration.png" alt="Manager Registration User Interface" id="enhancement-img2"></img>
+        <p id="enhancement-text2">Create a manager registration page with server side validation requiring unique
+            username and a password rule, and store this information in a table </p>
+        <div class='apply-btn'><a class='btn-gold' href='login.php' target='_blank' role='button'>Login Now</a></div>
     </div>
-    <div style="width:150px; height:150px; background-color:#CCCCFF; border:1px solid #999; text-align:center; line-height:150px;">
-        Box 3
+    <div id="enhancement-item3">
+        <h3>Managerial Login page</h3>
+        <img src="images/enhancements_img/manager_login.png" alt="Manager Login User Interface" id="enhancement-img3"></img>
+       <p id="enhancement-text3">Control access to manage.php by checking username and password. </p> <br> <br>
+       <div class='apply-btn'><a class='btn-gold' href='login.php' target='_blank' role='button'>Login Now</a></div>
+    </div>
+    <div id="enhancement-item4">
+        <h3>Login Attempt Limiter</h3>
+        <img src="images/enhancements_img/attempt_limiter.png" alt="Manager Login Attempt Limiter User Interface" id="enhancement-img4"></img>
+       <p id="enhancement-text4">Have access to the web site disabled for user a period of time on, say, three or more
+            invalid login attempts. </p> <br> 
+        <div class='apply-btn'><a class='btn-gold' href='login.php' target='_blank' role='button'>Login Now</a></div>
+    </div>
+    <div id="enhancement-item5">
+        <h3>Expression of Interest Deleter &amp; status updater</h3>
+        <img src="images/enhancements_img/eoi_statuses.png" alt="Expression of Interest Deleter &amp; Status Updater User Interface" id="enhancement-img5"></img>
+       <p id="enhancement-text5">Have access to the web site disabled for user a period of time on, say, three or more
+            invalid login attempts. </p>
+        <div class='apply-btn'><a class='btn-gold' href='login.php' target='_blank' role='button'>Login Now</a></div>
     </div>
 </div>
 
